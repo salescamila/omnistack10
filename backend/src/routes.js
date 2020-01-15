2cca016ad428560fb7d45b478570fc3bfe1d6543
Camila <salescamila1@gmail.com>
2020-01-14 22:51:30 -0400

    Pesquisa na api do github e salva infos na base

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,33 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/users/:id', (request, response) => {
-  console.log(request.body);
-  return response.json({ message: 'Hello OmniStack'});
+routes.post('/devs', async (request, response) => {
+  const { github_username, techs, latitude, longitude } = request.body;
+
+  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  const { name = login, avatar_url, bio } = apiResponse.data;
+
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  }
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+    location,
+  })
+
+  return response.json(dev);
 });
 
 module.exports = routes;
