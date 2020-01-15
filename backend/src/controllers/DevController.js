201db90d877eebdb6aa3b58afdbe0ae7d74a3ffd
Camila <salescamila1@gmail.com>
2020-01-14 23:04:23 -0400

    Verifica se usuário já foi cadastrado

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,25 +5,29 @@ module.exports = {
   async store (request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if(!dev){
+      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    const techsArray = techs.split(',').map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
+      const techsArray = techs.split(',').map(tech => tech.trim());
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      }
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      })
     }
-
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location,
-    })
 
     return response.json(dev);
   }
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,12 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 mongoose.connect(
     'mongodb://camila:camila@cluster0-shard-00-00-wj1qa.mongodb.net:27017,cluster0-shard-00-01-wj1qa.mongodb.net:27017,cluster0-shard-00-02-wj1qa.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   }).then(()=>console.log("DB server connect"))
     .catch(e => console.log("DB error", e));
 
