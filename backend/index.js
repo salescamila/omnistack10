f52de59a6710bceaca3e29c7689887b940261add
Camila <salescamila1@gmail.com>
2020-01-14 20:14:22 -0400

    Conectando a base de dados

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb://camila:<b455pl4yer>@cluster0-shard-00-00-wj1qa.mongodb.net:27017,cluster0-shard-00-01-wj1qa.mongodb.net:27017,cluster0-shard-00-02-wj1qa.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 
