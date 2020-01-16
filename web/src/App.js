a25f566d182873e6f953fe3ea37f80666c33c227
Camila <salescamila1@gmail.com>
2020-01-16 00:35:11 -0400

    Layout da lista de devs

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
@@ -36,7 +37,55 @@ function App() {
         </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/16194059?s=460&v=4" alt="Camila Sales" />
+              <div className="user-info">
+                <strong>Camila Sales</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Hi, nice to see you here :)</p>
+            <a href="https://github.com/salescamila">Acessar perfil no Github</a>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/16194059?s=460&v=4" alt="Camila Sales" />
+              <div className="user-info">
+                <strong>Camila Sales</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Hi, nice to see you here :)</p>
+            <a href="https://github.com/salescamila">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/16194059?s=460&v=4" alt="Camila Sales" />
+              <div className="user-info">
+                <strong>Camila Sales</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Hi, nice to see you here :)</p>
+            <a href="https://github.com/salescamila">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/16194059?s=460&v=4" alt="Camila Sales" />
+              <div className="user-info">
+                <strong>Camila Sales</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Hi, nice to see you here :)</p>
+            <a href="https://github.com/salescamila">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
