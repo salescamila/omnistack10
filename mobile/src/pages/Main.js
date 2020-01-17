a0d449cd58eb2f06e8856969f8e9b624680bd26a
Camila <salescamila1@gmail.com>
2020-01-17 12:43:29 -0400

    Exibir pop-up informativo ao clicar no avatar

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -37,6 +37,13 @@ function Main() {
     <MapView initialRegion={currentRegion} style={styles.map}>
       <Marker coordinate={{ latitude: currentRegion.latitude, longitude: currentRegion.longitude }}>
         <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/16194059?s=460&v=4' }} />
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Camila Sales</Text>
+            <Text style={styles.devBio}>Hi, nice to see you here :) I am currently doing the OmniStack from @Rockeseat</Text>
+            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+          </View>
+        </Callout>
       </Marker>
     </MapView>
   );
@@ -46,13 +53,32 @@ const styles = StyleSheet.create({
   map: {
     flex: 1
   },
+
   avatar: {
     width: 54,
     height: 54,
     borderRadius: 4,
     borderWidth: 4,
     borderColor: '#FFF'
-  }
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  debTechs: {
+    marginTop: 5,
+  },
 })
 
 export default Main;
