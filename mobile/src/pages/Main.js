9b6906412419c309bdc2f08f3603f6ae02841c69
Camila <salescamila1@gmail.com>
2020-01-18 17:08:25 -0400

    Adicionado componentes para buscar devs por techs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,21 +35,36 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: currentRegion.latitude, longitude: currentRegion.longitude }}>
-        <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/16194059?s=460&v=4' }} />
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: currentRegion.latitude, longitude: currentRegion.longitude }}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/16194059?s=460&v=4' }} />
 
-        <Callout onPress={() => {
-          navigation.navigate('Profile', { github_username: 'salescamila' });
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Camila Sales</Text>
-            <Text style={styles.devBio}>Hi, nice to see you here :) I am currently doing the OmniStack from @Rockeseat</Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'salescamila' });
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Camila Sales</Text>
+              <Text style={styles.devBio}>Hi, nice to see you here :) I am currently doing the OmniStack from @Rockeseat</Text>
+              <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={() => {}}  style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -81,6 +97,42 @@ const styles = StyleSheet.create({
 
   debTechs: {
     marginTop: 5,
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4DFF',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
   },
 })
 
